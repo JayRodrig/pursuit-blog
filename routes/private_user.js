90f04f2004de24e5b014eeef1aefd9df14ac2e64
JayRodrig <mrjosemlrod@gmail.com>
2019-01-24 20:36:15 -0500

    added all routes and functions to the private user paths

--- routes/private_user.js
+++ routes/private_user.js
@@ -1,21 +1,45 @@
-/*
-    USER ROUTES:
-        ❌ PUT /user/:user_id
-        ❌ DEL /user/:user_id
-*/
-
+// ----- DEPENDENCIES
 const PrivateUserRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const UserServices = require('../services/user_services');
 
+// ----- MIDDLEWARE
+PrivateUserRouter.use(UserServices.checkForToken);
+
+// ----- EXPRESS ROUTER CALLBACKS
 PrivateUserRouter.put('/:user_id', (request, response) => {
-    response.json({
-        'msg': `Successfully initialized your router.`
-    });
+    const {username, email, password,} = request.body;
+    const {user_id,} = request.params;
+    bcrypt.hash(password, 10)
+        .then(hash => {
+            return UserServices.updateUser(username, email, hash, user_id)
+        })
+        .then(() => {
+            response.json({
+                'msg': `You've successfully updated your user information.`
+            });
+        })
+        .catch(err => {
+            response.json({
+                'msg': `Something went wrong.`
+            });
+        });
+
 });
 
 PrivateUserRouter.delete('/:user_id', (request, response) => {
-    response.json({
-        'msg': `Successfully initialized your router.`
-    });
+    const {user_id,} = request.params;
+    UserServices.deleteUser(user_id)
+        .then(() => {
+            response.json({
+                'msg': `You've successfully deleted your user. Come back Soon!!`,
+            });
+        })
+        .catch(err => {
+            response.json({
+                'msg': `Something went wrong. Remember, before deleting a user you have to make sure you've deleted their posts/comments as well.`,
+            });
+        });
 });
 
 module.exports = PrivateUserRouter;
--- services/user_services.js
+++ services/user_services.js
@@ -1,16 +1,28 @@
+// ----- DEPENDENCIES
 const db = require('../db/database');
+
+// ----- GLOBAL VARIABLE
 const UserServices = {};
+
+// ----- OBJECT METHODS
+UserServices.login = (username) => db.one(
+    'SELECT username, password FROM users WHERE username = ${username}', {username,}
+);
 
 UserServices.create = (username, email, password) => db.none(
     'INSERT INTO users (username, email, password) VALUES (${username}, ${email}, ${password})', {username, email, password,}
 );
 
-UserServices.login = (username) => db.one(
-    'SELECT username, password FROM users WHERE username = ${username}', {username,}
-);
-
 UserServices.readUser = (user_id) => db.one(
     'SELECT * FROM users WHERE id = ${user_id}', {user_id,}
+);
+
+UserServices.updateUser = (username, email, password, user_id) => db.none(
+    'UPDATE users SET username = ${username}, email = ${email}, password = ${password} WHERE users.id = ${user_id}', {username, email, password, user_id,}
+);
+
+UserServices.deleteUser = (user_id) => db.none(
+    'DELETE FROM users WHERE users.id = ${user_id}', {user_id,}
 );
 
 UserServices.readPosts = (user_id) => db.any(
@@ -29,8 +41,18 @@ UserServices.readComment = (user_id, comment_id) => db.one(
     'SELECT users.username, comments.title, comments.body FROM users JOIN comments ON users.id = comments.author WHERE author = ${user_id} AND comments.id = ${comment_id}', {user_id, comment_id,}
 );
 
+
 UserServices.insertToken = (token, username) => db.none(
     'UPDATE users SET token = ${token} WHERE username = ${username}', {token, username,}
 );
+
+UserServices.checkForToken = (request, response, next) => {
+    if (!(request.headers.token)) {
+        response.json({
+            'msg': `You haven't logged in YET.`
+        });
+    }
+    next();
+}
 
 module.exports = UserServices;
